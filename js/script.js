b9c0169f1a779f3a933bd183129fdc2592441371
lamr64 <112106063+lamr64@users.noreply.github.com>
2026-03-09 01:04:19 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -92,6 +92,39 @@ overlay.remove();
 
 });
 
+// маска телефона
+
+const phoneInput = document.getElementById("phone");
+
+phoneInput.addEventListener("input", function(){
+
+let numbers = this.value.replace(/\D/g,'');
+
+if(numbers.startsWith('8')){
+numbers = numbers.substring(1);
+}
+
+if(numbers.startsWith('7')){
+numbers = numbers.substring(1);
+}
+
+let formatted = "+7 ";
+
+if(numbers.length > 0)
+formatted += "(" + numbers.substring(0,3);
+
+if(numbers.length >= 3)
+formatted += ") " + numbers.substring(3,6);
+
+if(numbers.length >= 6)
+formatted += "-" + numbers.substring(6,8);
+
+if(numbers.length >= 8)
+formatted += "-" + numbers.substring(8,10);
+
+this.value = formatted;
+
+});
 
 
 // ===== TELEGRAM ФОРМА =====
